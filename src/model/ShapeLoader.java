a1cf92e1cfe06712339011ac214e8f5b32a96a92
Saif eldeen Adel <saifadel77@hotmail.com>
2023-12-18 16:30:34 +0200

    Shape loader works, removed redundancies

--- src/model/ShapeLoader.java
+++ src/model/ShapeLoader.java
@@ -7,73 +7,44 @@ import java.io.IOException;
 import java.util.*;
 
 public class ShapeLoader {
-    // List to store the classes that contains the shapes to chooses randomly from when uploading
-    private List<Class<? extends Shape>> loadedClass;
-    // Set to store the loaded classes just making sure no duplicates
-    private Set<Class<? extends Shape>> set;
     // To map between images in the file and BufferedImage objects
     private Map<Class<? extends Shape>, BufferedImage[]> map;
     // To get the name of the folder
-    private File folderName;
+    private File folder;
+    private String currentLoading;
+
+    public void setCurrentLoading(String currentLoading) {
+        this.currentLoading = currentLoading;
+    }
 
     /*
      * Constructor
      */
-    public ShapeLoader(List<Class<? extends Shape>> loadedClass, Set<Class<? extends Shape>> set,Map<Class<? extends Shape>, BufferedImage[]> map, File folderName) {
-        this.loadedClass = loadedClass;
-        this.set = new HashSet<>();
-        this.map = map;
-        this.folderName = folderName;
+    public ShapeLoader() {
+        this.map = new HashMap<>();
+        this.folder = new File("src/model/Shapes");
     }
 
-    public void setFolderName(File folderName) {
-        this.folderName = folderName;
+    public void setFolder(File folder) {
+        this.folder = folder;
     }
 
     /*
      * no use till now
      */
     public String getClassName(String fileName) {
-        return fileName.replace(".java", "");
+        setCurrentLoading(fileName.replace(".java", ""));
+        return currentLoading;
     }
 
-    /*
-     * This function takes folder name and iterates
-     * through its contents either files or sub-folders
-     */
-//    public void loadFolder() {
-//        try {
-//            File[] files = folderName.listFiles();
-//            for (File file : files) {
-//                // If directory or folder the function is called recursively
-//                if (file.isDirectory()) {
-//                    setFolderName(file);  // Not sure if it works when recursive or not
-//                    loadFolder();
-//                    System.out.println("Folder Name: " + file.getName());
-//                }
-//                // If not directory the loadClass function is called and do some checkings to add the file in the list
-//                else if (file.isFile() && file.getName().endsWith(".java")) {
-//                    loadClass(file);
-//                    System.out.println("Java File: " + file.getName());
-//                }
-//                // If the file is Image
-//                else {
-//                    loadImage(file);
-//                    System.out.println("Image Name: " + file.getName());
-//                }
-//            }
-//        } catch (NullPointerException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
-    public void loadFolder(){
+    public Map<Class<? extends Shape>, BufferedImage[]> loadFolder(){
         try {
-            File[] files = folderName.listFiles();
+            File[] files = folder.listFiles();
             if (files != null) {
                 for (File file : files) {
                     if (file.isDirectory()) {
-                        setFolderName(file);
+                        setFolder(file);
                         loadClassFiles();
                         loadImageFiles();
                     }
@@ -82,6 +53,7 @@ public class ShapeLoader {
         } catch (NullPointerException e) {
             e.printStackTrace();
         }
+        return this.map;
     }
 
     /*
@@ -89,7 +61,7 @@ public class ShapeLoader {
      * from the current shape folder
      */
     public void loadClassFiles() {
-        File[] classFiles = folderName.listFiles(file -> file.isFile() && file.getName().endsWith(".java"));
+        File[] classFiles = folder.listFiles(file -> file.isFile() && file.getName().endsWith(".java"));
         if (classFiles != null) {
             for (File classFile : classFiles) {
                 loadClass(classFile);
@@ -102,7 +74,7 @@ public class ShapeLoader {
      * from the current shape folder
      */
     public void loadImageFiles() {
-        File[] imageFiles = folderName.listFiles(file -> file.isFile() && file.getName().endsWith(".png"));
+        File[] imageFiles = folder.listFiles(file -> file.isFile() && file.getName().endsWith(".png"));
         if (imageFiles != null) {
             for (File imageFile : imageFiles) {
                 loadImage(imageFile);
@@ -117,10 +89,8 @@ public class ShapeLoader {
     public void loadClass(File file) {
         try {
             String className = getClassName(file.getName());
-            Class<?> cls = Class.forName(className);
-            if (!set.contains(cls) && Shape.class.isAssignableFrom(cls)) {
-                loadedClass.add((Class<? extends Shape>) cls);
-                set.add((Class<? extends Shape>) cls);
+            Class<?> cls = Class.forName("model.Shapes." + className + "." + className);
+            if (!map.containsKey(cls) && Shape.class.isAssignableFrom(cls)) {
                 map.put((Class<? extends Shape>) cls, new BufferedImage[0]); // Initialize the array for images
             }
         } catch (ClassNotFoundException e) {
@@ -134,18 +104,23 @@ public class ShapeLoader {
     public void loadImage(File image) {
         try {
             BufferedImage bufferedImage = ImageIO.read(image);
-            String className = getClassName(image.getName());
-            Class<? extends Shape> cls = (Class<? extends Shape>) Class.forName(className);
-
+            Class<? extends Shape> cls = (Class<? extends Shape>) Class.forName("model.Shapes." + currentLoading + "." + currentLoading);
             if (map.containsKey(cls)) {
                 BufferedImage[] images = map.get(cls);
                 map.put(cls, Arrays.copyOf(images, images.length + 1));
                 map.get(cls)[images.length] = bufferedImage;
+                System.out.println(bufferedImage.getWidth());
             } else {
                 map.put(cls, new BufferedImage[]{bufferedImage});
             }
         } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        ShapeLoader loader = new ShapeLoader();
+        Map<Class<? extends Shape>, BufferedImage[]> loadedMap = loader.loadFolder();
+        System.out.println(loadedMap.keySet());
     }
 }
